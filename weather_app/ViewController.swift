3f0af18fb69f0adf9c5cc50e9e312c9056572538
Robert Menke <robert.b.menke@gmail.com>
2017-10-06 13:10:05 -0400

    Initial Commit

---
+++ weather_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather_app
+//
+//  Created by Robert B. Menke on 10/6/17.
+//  Copyright © 2017 Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
